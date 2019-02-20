5129100a39c480ac6c7be13484311cd569bac171
Matthias Hannig <mail@matthiashannig.de>
2019-02-20 10:56:31 +0100

    added example service

---
+++ src/examples/user_svc.py
@@ -0,0 +1,46 @@
+
+"""
+A simple example for a command and multiple handlers
+"""
+
+import random
+
+import active
+
+class CreateUserRequest(active.Action): pass
+class CreateUserSuccess(active.Action): pass
+class CreateUserError(active.Action): pass
+
+
+@active.command
+def create_user(dispatch, request):
+    """Create a user"""
+    if request.username == "admin":
+        print("Not creating user because reason.")
+        dispatch(CreateUserError(message="Can not be admin!"))
+        return
+
+    print(f"Creating user: {request.username}")
+    # ... rest of the owl
+    user_id = random.randint(230, 420)
+    user = {
+        "username": request.username,
+        "id": user_id,
+    }
+
+    dispatch(CreateUserSuccess(user=user))
+
+
+@active.handler(
+    CreateUserSuccess,
+    CreateUserError)
+def update_user_state(action):
+    """Something happened. Update some state."""
+    print("Updating user state with action:")
+    print(action)
+
+
+@active.handler(CreateUserSuccess)
+def log_succes(action):
+    """User was successfully created"""
+    print("Created user:", action.user)
