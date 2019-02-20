7bf2a2f799295f32f73afc33a774ee834fc00019
Matthias Hannig <mail@matthiashannig.de>
2019-02-20 10:55:57 +0100

    added command wrapper

---
+++ src/active/commands.py
@@ -0,0 +1,30 @@
+
+"""
+Active Command
+--------------
+
+Decorate a function to act as a command.
+This function will receive a dispatch function
+to emit events.
+"""
+
+from functools import wraps
+
+from active.dispatch import make_dispatch
+
+def command(cmd):
+    """
+    Decorate a command function and provide a dispatch.
+    Make sure the function is unary and only
+    accepts a Action.
+    """
+    # Prepare dispatch function
+    dispatch = make_dispatch(cmd)
+
+    @wraps(cmd)
+    def _command_wrapper(request):
+        """Accept the request and call wrapped function"""
+        return cmd(dispatch, request)
+    return _command_wrapper
+
+
